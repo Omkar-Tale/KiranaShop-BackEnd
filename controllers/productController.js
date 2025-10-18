5fadaa7dd136a4b3732bc477b677aaabfbb5e714
Omkar Tale <omkarktale1603@gmail.com>
2025-10-18 12:34:46 +0530

    product api half done

---
+++ controllers/productController.js
@@ -0,0 +1,20 @@
+
+// add products by seller : path = api/product/addProduct
+export const addProduct = async (req, res)=>{
+
+}
+
+// get product list : path = api/product/list
+export const productList = async (req, res)=>{
+
+}
+
+// get signle product by id : path = api/product/id
+export const productById = async (req, res)=>{
+
+}
+
+// change inStock: path = api/product/stock
+export const changeStock = async (req, res)=>{
+
+}
