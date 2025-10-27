64f8066fc0f4d88572188423c74685dd06c0494d
Omkar Tale <omkarktale1603@gmail.com>
2025-10-27 11:17:18 +0530

    final changes done

--- config/multer.js
+++ config/multer.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-import multer from 'multer'
-=======
+
 import multer from "multer";
->>>>>>> bae04014d289be2616c472ed0c5aeb4e86e43fdc
 
 export const upload = multer({storage: multer.diskStorage({})})
--- controllers/productController.js
+++ controllers/productController.js
@@ -1,54 +1,6 @@
 import {v2 as cloudinary} from "cloudinary"
 import { productModel } from "../models/ProductModel.js";
-<<<<<<< HEAD
 
-
-// add products by seller : path = api/product/addProduct
-export const addProduct = async (req, res)=>{
-   try {
-    // it converts the string into js object
-    let productData = JSON.parse(req.body.productData)
-
-    // to get the uploaded images from multer
-    const images = req.files;
-
-    // function of converting image into urls by cloudinary
-    let imageUrl = await Promise.all(
-        images.map( async (item) => {
-            let result = await cloudinary.uploader.upload(item.path, {resource_type: "image"});
-            return result.secure_url;
-        })
-    )
-
-    // save to the mongodb
-    await productModel.create({...productData, image: imageUrl});
-
-    res.json({
-        message: "Image Added!"
-    })
-
-   } catch (error) {
-    console.log(error.message);
-    res.json({
-        message: error.message
-    })    
-   }
-}
-
-// get product list : path = api/product/list
-export const productList = async (req, res) => {
-    try {
-        const productList = await productModel.find();
-        res.json({
-            message: "List of all products",
-            productList
-        })
-    } catch (error) {
-        console.log(error.message);
-        res.json({
-            message: error.message
-        })
-=======
 
 // add products by seller : path = api/product/addProduct
 export const addProduct = async (req, res)=>{
@@ -83,7 +35,6 @@ export const productList = async (req, res)=>{
         })
     } catch (error) {
         console.log(error.message)
->>>>>>> bae04014d289be2616c472ed0c5aeb4e86e43fdc
     }
 }
 
@@ -91,27 +42,12 @@ export const productList = async (req, res)=>{
 export const productById = async (req, res)=>{
     try {
         const {id} = req.body
-
-<<<<<<< HEAD
-        const product = await productModel.findById(id);
-
-=======
-    try {
-        const {id} = req.body
-        const product = await productModel.findById(id);
->>>>>>> bae04014d289be2616c472ed0c5aeb4e86e43fdc
+        const product = await productModel.findById(id)
         res.json({
             product
         })
     } catch (error) {
-<<<<<<< HEAD
-        console.log(error.message);
-        res.json({
-            message: error.message
-        })
-=======
         console.log(error.message)
->>>>>>> bae04014d289be2616c472ed0c5aeb4e86e43fdc
     }
 }
 
@@ -119,21 +55,6 @@ export const productById = async (req, res)=>{
 export const changeStock = async (req, res)=>{
     try {
         const {id, inStock} = req.body
-<<<<<<< HEAD
-
-        await productModel.findByIdAndUpdate(id, {inStock})
-
-        res.json({
-            message: "product stock updated!"
-        })
-    } catch (error) {
-        console.log(error.message);
-        res.json({
-            message: error.message
-        })
-    }
-}
-=======
         await productModel.findByIdAndUpdate(id, {inStock});
         res.json({
             message: "stock updated!"
@@ -141,5 +62,4 @@ export const changeStock = async (req, res)=>{
     } catch (error) {
         console.log(error.message)
     }
-}                     
->>>>>>> bae04014d289be2616c472ed0c5aeb4e86e43fdc
+}
