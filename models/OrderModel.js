732e46e3824db848124c9e0b00f77e4f0397aa30
Omkar Tale <omkarktale1603@gmail.com>
2025-10-27 13:45:05 +0530

    .

---
+++ models/OrderModel.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose'
+
+const orderSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+        ref: "user"
+    },
+    items: [{
+        product: {
+            type: String,
+            required: true,
+            ref: 'user'
+        },
+        quantity: {
+            type: Number,
+            required: true
+        }
+    }],
+    amount: {
+        type: Number,
+        required: true
+    },
+    address: {
+        type: Number,
+        required: true,
+        ref: 'address'
+    },
+    status: {
+        type: Number,
+        default: "Order Placed"
+    },
+    paymentType: {
+        type: String,
+        required: true
+    },
+    isPaid: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+},{timestamps: true})
+
+export const orderModel = mongoose.model.order || mongoose.model("order", orderSchema)
